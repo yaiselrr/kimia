2feee18f45c81b0f104d991d93809a12ccd81b34
Yaisel Rodriguez Regueras <laraveldev3@gmail.com>
2024-02-18 14:20:36 -0500

    insert team ok

--- src/Controller/Api/TeamController.php
+++ src/Controller/Api/TeamController.php
@@ -2,20 +2,56 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Team;
+use App\Form\Type\TeamFormType;
 use App\Repository\TeamRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class TeamController extends AbstractFOSRestController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
     /**
      * 
      * @Rest\Get(path="/teams")
      * @Rest\View(serializerGroups={"team"},serializerEnableMaxDepthChecks=true)
      */
-    public function getActions(TeamRepository $repo)
+    public function index(TeamRepository $repo)
     {
+        $this->logger->info('list action callled');
+
         return $repo->findAll();
+    }
+
+    /**
+     * 
+     * @Rest\Post(path="/teams/new")
+     * @Rest\View(serializerGroups={"team"},serializerEnableMaxDepthChecks=true)
+     */
+    public function store(Request $request, EntityManagerInterface $em)
+    {
+        $this->logger->info('create action callled');
+
+        $team = new Team();
+        $form = $this->createForm(TeamFormType::class, $team);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($team);
+            $em->flush();
+
+            return $team;
+        }
+
+        return $form;
     }
 }
---
+++ src/Form/Type/TeamFormType.php
@@ -0,0 +1,27 @@
+<?php
+
+// src/Form/Type/TaskType.php
+namespace App\Form\Type;
+
+use App\Entity\Team;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TeamFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('color', TextType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Team::class,
+        ]);
+    }
+}
